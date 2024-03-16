9f6541f8d1ac4fdbbfb506f5992473831fbb6089
Raman Adlakha <radlakha@users.noreply.github.com>
2024-03-16 14:12:08 +0000

    Ignore whitespace in Incomplete syntax

--- src/rules/checkIncompleteSyntax.ts
+++ src/rules/checkIncompleteSyntax.ts
@@ -11,7 +11,7 @@ import { nodeToString } from "@/util/nodeToString";
  *  %% INCOMPLETE(issue which is a string) %%
  * %% INCOMPLETE %%
  */
-const syntaxRegex = /%%\s*INCOMPLETE(?:\(([^)]+)\))?\s*%%/g;
+const syntaxRegex = /%%\s*INCOMPLETE\s*(?:\(([^)]+)\))?\s*%%/g;
 
 export const checkIncompleteSyntax: IssueScanner = {
 	icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-x"><path d="M11 12H3"/><path d="M16 6H3"/><path d="M16 18H3"/><path d="m19 10-4 4"/><path d="m15 10 4 4"/></svg>`,
